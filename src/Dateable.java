51ab3c8cba638e733d086541af30119759f9b39b
awoodrey <audreyw2026@student.cis.edu.hk>
2024-01-26 10:14:03 +0800

    Finished Lab

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("PLease enter your age:");
+        int age = scanner.nextInt();
+        int newage = age/2 + 7;
+        System.out.println(age + "-year olds should date somebody who is at least " + newage + " years old");
     }
 }
