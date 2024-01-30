f3aa3c5104aa898470f900b99f2725a138d55678
awoodrey <audreyw2026@student.cis.edu.hk>
2024-01-30 11:43:02 +0800

    Finished Lab

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("PLease enter your age:");
+        int age = scanner.nextInt();
+        int newage = 100-age;
+        System.out.println(age + "-year olds should read at least " + newage + " pages before giving up on a book");
     }
 }
